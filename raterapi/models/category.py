a4315091aa8402e3786b2d697d7da3c23c05a917
William Woodard <williamwoodard98@gmail.com>
2024-06-19 14:46:09 -0500

    fixtures created

--- raterapi/models/category.py
+++ raterapi/models/category.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=155)
+    game = models.ManyToManyField(
+        'Game', 
+        through="GameCategory",
+        related_name="categories"
+        )
