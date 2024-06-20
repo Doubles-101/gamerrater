a1bc5e147bf8f522d537a9818b0ec07d61fe08b3
William Woodard <williamwoodard98@gmail.com>
2024-06-20 15:11:23 -0500

    added game view, can GET

---
+++ raterapi/views/game_view.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets, status, serializers, permissions
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from raterapi.models import Rating, Review, Image, Game
+
+class GameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Game
+        fields = ['id', 'title', 'description', 'designer', 'year_released', 'number_of_players', 'estimated_time_to_play', 'age_recommendation']
+
+
+
+class GameViewSet(ViewSet):
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        try:
+            games = Game.objects.get(pk=pk)
+            serializer = GameSerializer(games, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Game.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
